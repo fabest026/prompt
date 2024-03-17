7d495a63c9cf634d24bdc48543b938823bfc933b
Muhammad Farhan Akbar <161922838+fabest026@users.noreply.github.com>
2024-03-17 23:49:24 +0500

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -115,7 +115,7 @@ with st.sidebar:
     writing_styles = st.sidebar.selectbox("Choose Writing Styles:", ["Academic", "Conversational", "Creative", "Critical", "Descriptive", "Instructive", "Technical", "Analytical","Business", "Causal", "Emotional", "Expository", "Formal", "Informal", "Legal", "Medical", "Poetic", "Persuasive"])
     
     # Audience
-    audience = st.selectbox("Audience: [Who is the target audience?]", ["Teenager", "Adult", "20-years-old", "30-years-old",  "40-years-old", "50-years-old", "Senior", "Everyone", "Uninformed Audience", "Neutral Audience", "Business Audience", "Researcher", "Expert Audience", "My Boss", "My Student", "My Teacher", "My Family", "My Friends", "My Colleagues"] )
+    audience = st.selectbox("Audience: Who is the target audience?", ["Teenager", "Adult", "20-years-old", "30-years-old",  "40-years-old", "50-years-old", "Senior", "Everyone", "Uninformed Audience", "Neutral Audience", "Business Audience", "Researcher", "Expert Audience", "My Boss", "My Student", "My Teacher", "My Family", "My Friends", "My Colleagues"] )
     
     # Word Counter
     word_count = st.number_input("Word Count", min_value=10, max_value=3000, step=100)
