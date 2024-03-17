6685bece275b7098e82f8ea503eb1c309f2ed413
Muhammad Farhan Akbar <161922838+fabest026@users.noreply.github.com>
2024-03-17 23:50:07 +0500

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -106,7 +106,7 @@ with st.sidebar:
     topic = st.text_input("Topic: What is the topic of the prompt? Eg: python, machine learning,..")
     
     # Goal
-    task = st.text_area(" Task: What do you want to achieve with the prompt? Eg: writing a blog, analyzing a text,..")
+    task = st.text_input(" Task: What do you want to achieve with the prompt? Eg: writing a blog, analyzing a text,..")
     
     # Add the Voice Tones
     voice_tones = st.sidebar.selectbox("Choose Voice Tones:", ["Formal","Friendly", "Bold", "Adventurous", "Witty", "Professional", "Casual", "Informative", "Creative", "Trendy", "Caring", "Cheerful", "Excited", "Funny", "Sad", "Serious", "Tense", "Vulnerable", "Angry", "Surprised", "Worried", "Assertive", "Confident", "Cooperative", "Encouraging" ])
